1aad764375721a5563874f7096d18c8e883c3416
Walmer Almeida Cavalcante <walmeralmeidacavalcante@gmail.com>
2023-01-15 17:48:25 -0300

    Projeto

---
+++ primeiro-proj/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component está aqui!</p>
+    `
+})
+export class MeuPrimeiroComponent {}
